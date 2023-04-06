a6206ff8689915d30f19061bc0b3d3d600beb17e
renyusuke <86160603+renyusuke@users.noreply.github.com>
2023-04-06 20:40:30 +0800

    update

--- tbl/route_tbl.go
+++
@@ -1,9 +0,0 @@
-package tbl
-
-type Route struct {
-	Model
-	RouteName string `gorm:"type:varchar(20)"`
-	RoutePath string `gorm:"type:varchar(40)"`
-	CreatedAt int64  `gorm:"type:int(12)"`
-	UpdatedAt int64  `gorm:"type:int(12)"`
-}
