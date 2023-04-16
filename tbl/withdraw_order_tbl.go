034f81ba18a2ed74df38d7a63262dd98b60b0359
renyusuke <86160603+renyusuke@users.noreply.github.com>
2023-04-16 16:16:25 +0800

    加上定義

--- tbl/withdraw_order_tbl.go
+++ tbl/withdraw_order_tbl.go
@@ -5,7 +5,9 @@ type WithdrawOrder struct {
 	OrderNo         string `gorm:"type:varchar(60)"` // 我方訂單編號
 	MerchantNo      string `gorm:"type:varchar(60)"` // 平台號
 	Type            int64  `gorm:"type:int(12)"`     // 訂單模式
-	Status          int64  `gorm:"type:int(12)"`     // 訂單裝太
+	Status          int64  `gorm:"type:int(12)"`     // 訂單狀態
+	FromCurrencyId  int64  `gorm:"type:int(12)"`     // 異動幣種
+	ToCurrencyId    int64  `gorm:"type:int(12)"`     // 兌換幣種
 	Sign            string `gorm:"type:varchar(60)"` // 加密sign
 	Shift           string `gorm:"type:varchar(40)"` //更次
 	ClientCreatedAt int64  `gorm:"type:int(12)"`     //平台創建時間
