dc2ce23b6d9edc3a87e65655540658f9309f2685
renyusuke <86160603+renyusuke@users.noreply.github.com>
2023-04-28 18:14:50 +0800

    新增商戶操作人表

--- tbl/admin_tbl.go
+++ tbl/admin_tbl.go
@@ -3,9 +3,13 @@ package tbl
 // Admin 商戶操作人
 type Admin struct {
 	Model
-	Name        int64 `gorm:"type:varchar(20);comment:'名稱'"`
-	AdminType   int64 `gorm:"type:int(12);comment:'後台使用者類別'"`
-	AdminStatus int64 `gorm:"type:int(12);comment:'後台使用者狀態'"`
-	UpdatedAt   int64 `gorm:"autoUpdateTime:milli;comment:'更新時間'"`
-	CreatedAt   int64 `gorm:"type:int(14);comment:'創建時間'"`
+	Name         string `gorm:"type:varchar(20)"`
+	Type         int64  `gorm:"type:int(12)"`
+	Status       int64  `gorm:"type:int(12)"`
+	Account      string `gorm:"type:varchar(20)"`
+	Pass         string `gorm:"type:varchar(20)"`
+	GToken       string `gorm:"type:varchar(50)"`
+	SecondSecure int64  `gorm:"type:int(12)"`
+	UpdatedAt    int64  `gorm:"autoUpdateTime:milli;comment:'更新時間'"`
+	CreatedAt    int64  `gorm:"type:int(14);comment:'創建時間'"`
 }
