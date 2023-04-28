76984b7edfd82a6b7d5cdb6dbb580363da5c1835
renyusuke <86160603+renyusuke@users.noreply.github.com>
2023-04-28 19:25:55 +0800

    增加錯誤判斷

--- enums/err.go
+++ enums/err.go
@@ -3,4 +3,8 @@ package enums
 // ErrAdminTokenExpired Admin 後台相關 0000 - 0999
 const (
 	ErrAdminTokenExpired = 10001 //token 過期
+	ErrVerifyFail        = 10002 //驗證錯誤
+	ErrAccountFreeze     = 10003 //帳號已凍結
+	ErrNameDuplicated    = 20001 //名稱重複
+	ErrPassNotEnough     = 20002 //密碼長度不足
 )
--- lan/cn/err.go
+++ lan/cn/err.go
@@ -1,10 +1,23 @@
 package cn
 
-import "errors"
+import (
+	"brunnen/enums"
+	"errors"
+)
 
 var (
-	ErrMap = map[int64]error{
-		10001: errors.New("token 失效，請重新登入"),
-	}
 	ErrAdminTokenExpired = errors.New("token 失效，請重新登入")
+	ErrVerifyFail        = errors.New("驗證失敗")
+	ErrAccountFreeze     = errors.New("已凍結")
+
+	ErrDuplicatedName = errors.New("名稱重複")
+	ErrPassNotEnough  = errors.New("密碼長度不夠")
+
+	ErrMap = map[int64]error{
+		enums.ErrAdminTokenExpired: ErrAdminTokenExpired,
+		enums.ErrVerifyFail:        ErrVerifyFail,
+		enums.ErrAccountFreeze:     ErrAccountFreeze,
+		enums.ErrNameDuplicated:    ErrDuplicatedName,
+		enums.ErrPassNotEnough:     ErrPassNotEnough,
+	}
 )
