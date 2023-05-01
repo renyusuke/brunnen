2f36ce76b0acfcf99858515e9c7f98c110d5ace7
renyusuke <86160603+renyusuke@users.noreply.github.com>
2023-05-01 20:18:12 +0800

    加上SetLock 輔助方法

---
+++ helper/lock.go
@@ -0,0 +1,39 @@
+package helper
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+	"time"
+
+	"context"
+)
+
+var (
+	key        = "public:lock:%v"
+	expiration = 10 * time.Second
+)
+
+func SetLock(ctx *gin.Context, r *redis.ClusterClient, val interface{}) bool {
+
+	k := fmt.Sprintf(key, val)
+	err := r.SetNX(ctx, key, 0, expiration).Err()
+	if err != nil {
+		return false
+	}
+
+	go func(k string) {
+		defer r.Del(context.Background(), k).Err()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+
+			case <-time.After(expiration):
+				return
+			}
+		}
+	}(k)
+
+	return true
+}
