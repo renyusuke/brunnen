49ca72b828b115219dabc6192d310efd85b645a5
renyusuke <86160603+renyusuke@users.noreply.github.com>
2023-05-01 16:57:24 +0800

    加入md5

--- helper/comman.go
+++ helper/comman.go
@@ -1,6 +1,8 @@
 package helper
 
 import (
+	"crypto/md5"
+	"fmt"
 	"github.com/renyusuke/brunnen/lan/cn"
 	"github.com/renyusuke/brunnen/vars"
 )
@@ -11,3 +13,10 @@ var (
 	DefaultAdminType    = cn.AdminTypeOperator
 	DefaultWalletStatus = cn.WalletStatusNormal
 )
+
+func Md5(str string) string {
+	data := []byte(str)
+	has := md5.Sum(data)
+	md5str := fmt.Sprintf("%x", has) //将[]byte转成16进制
+	return md5str
+}
