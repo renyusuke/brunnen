f336963685bf030c861686374e7eabf5f18e0c7b
renyusuke <86160603+renyusuke@users.noreply.github.com>
2023-05-02 18:49:31 +0800

    加上key

--- helper/cache.go
+++ helper/cache.go
@@ -3,8 +3,6 @@ package helper
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/redis/go-redis/v9"
-	"github.com/renyusuke/brunnen/tool"
-	"strconv"
 	"time"
 )
 
@@ -13,13 +11,13 @@ var (
 )
 
 func GetToken(ctx *gin.Context, rdb *redis.Client, key string) (res bool, err error, token string) {
-	err = rdb.Get(ctx, key).Err()
+	token, err = rdb.Get(ctx, key).Result()
+	if err != nil {
+		panic(err)
+	}
 	if err != nil {
 		return false, err, ""
 	} else {
-		w := tool.Worker{}
-		tokenInt64 := w.GetId()
-		token = strconv.FormatInt(tokenInt64, 10)
 		err = SetToken(ctx, rdb, key, token)
 		if err != nil {
 			return false, err, ""
