417ba595595ca85a8c6384929d3492223173a99e
renyusuke <86160603+renyusuke@users.noreply.github.com>
2023-05-03 14:15:04 +0800

    加上創建人字段

--- tbl/admin_tbl.go
+++ tbl/admin_tbl.go
@@ -10,6 +10,7 @@ type Admin struct {
 	Pass         string `gorm:"type:varchar(40)"`
 	GToken       string `gorm:"type:varchar(50)"`
 	SecondSecure int64  `gorm:"type:bigint(30)"`
+	CreatorName  string `gorm:"type:varchar(40)"`
 	UpdatedAt    int64  `gorm:"autoUpdateTime:milli;comment:'更新時間'"`
 	CreatedAt    int64  `gorm:"type:bigint(30);comment:'創建時間'"`
 }
--- tbl/client_tbl.go
+++ tbl/client_tbl.go
@@ -2,9 +2,10 @@ package tbl
 
 type Client struct {
 	Model
-	Name         int64 `gorm:"type:varchar(20);comment:'名稱'"`
-	ClientType   int64 `gorm:"type:int(12);comment:'商戶類別'"`
-	ClientStatus int64 `gorm:"type:int(12);comment:'商戶狀態'"`
-	UpdatedAt    int64 `gorm:"autoUpdateTime:milli;comment:'更新時間'"`
-	CreatedAt    int64 `gorm:"type:int(14);comment:'創建時間'"`
+	Name         int64  `gorm:"type:varchar(20);comment:'名稱'"`
+	ClientType   int64  `gorm:"type:int(12);comment:'商戶類別'"`
+	ClientStatus int64  `gorm:"type:int(12);comment:'商戶狀態'"`
+	CreatorName  string `gorm:"type:varchar(40)"`
+	UpdatedAt    int64  `gorm:"autoUpdateTime:milli;comment:'更新時間'"`
+	CreatedAt    int64  `gorm:"type:int(14);comment:'創建時間'"`
 }
