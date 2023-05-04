98ddebfecf2db6c674eab94732e01a3a37faa664
renyusuke <86160603+renyusuke@users.noreply.github.com>
2023-05-04 12:26:18 +0800

    加上權限

--- tbl/route_for_admin_tbl.go
+++ tbl/route_for_admin_tbl.go
@@ -3,7 +3,7 @@ package tbl
 type RouteForAdmin struct {
 	Id    int64  `gorm:"type:int(50);primaryKey;unique;not null"`
 	Sign  int64  `gorm:"type:int(50);unique"`
-	Title string `gorm:"type:int(12);comment:'標題 default 中文'"`
+	Title string `gorm:"type:varchar(12);comment:'標題 default 中文'"`
 	Type  int64  `gorm:"type:int(12);comment:'1:主條目 2:頁面 3:按鍵'"`
 	Path  string `gorm:"type:varchar(40);comment:'1:主條目 2:頁面 3:按鍵'"`
 }
