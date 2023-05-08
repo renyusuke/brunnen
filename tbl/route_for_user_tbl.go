2f6f8fbc16ce0a6a1703e8038c1a30756471e3e1
renyusuke <86160603+renyusuke@users.noreply.github.com>
2023-05-08 15:13:30 +0800

    新稱transaction table

--- tbl/route_for_user_tbl.go
+++ tbl/route_for_user_tbl.go
@@ -1,7 +1,9 @@
 package tbl
 
 type RouteForUser struct {
-	id    int64
-	title string
-	path  string
+	Id    int64  `gorm:"type:int(50);primaryKey;unique;not null"`
+	Sign  int64  `gorm:"type:int(50)"`
+	Title string `gorm:"type:varchar(12);comment:'標題 default 中文'"`
+	Type  int64  `gorm:"type:int(12);comment:'1:主條目 2:頁面 3:按鍵'"`
+	Path  string `gorm:"type:varchar(40);comment:'路徑'"`
 }
