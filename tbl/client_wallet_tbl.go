d71f5969aab937c0fcffbe1d5c038353ff290478
renyusuke <86160603+renyusuke@users.noreply.github.com>
2023-05-08 15:57:06 +0800

    wallet 不需另外綁定

--- tbl/client_wallet_tbl.go
+++
@@ -1,9 +0,0 @@
-package tbl
-
-// ClientWallet 商戶綁定錢包
-type ClientWallet struct {
-	Model
-	ClientId  int64 `gorm:"type:varchar(20);comment:'客戶ID'"`
-	WalletId  int64 `gorm:"type:int(12);comment:'錢包ID'"`
-	CreatedAt int64 `gorm:"type:int(14);comment:'創建時間'"`
-}
