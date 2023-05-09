68ea2a5c5018b940b0dc81a2ee70bcd13dded06e
renyusuke <86160603+renyusuke@users.noreply.github.com>
2023-05-09 17:00:49 +0800

    OperatorName -> Operator

--- helper/timex/timex.go
+++ helper/timex/timex.go
@@ -1,7 +1,10 @@
 package timex
 
 import (
+	"fmt"
 	"github.com/golang-module/carbon/v2"
+
+	"time"
 )
 
 // GetMonthBetweenByStr 获取指定时间月份区间
@@ -16,4 +19,11 @@ func GetMonthBetween() (int64, int64) {
 	start := carbon.Now().StartOfMonth().Millisecond()
 	end := carbon.Now().EndOfMonth().Millisecond()
 	return int64(start), int64(end)
+}
+
+// TimeStampFormatString 時間戳format 字串
+func TimeStampFormatString(timeStr int64, formatKey string) (res string) {
+	tm := time.Unix(timeStr, 0).Format(formatKey)
+	res = fmt.Sprintf("%v", tm)
+	return res
 }
