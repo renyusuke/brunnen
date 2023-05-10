bb6b6a876c1e31be9f5c142757030dc6abb67f3b
renyusuke <renyusuke1990@gmail.com>
2023-05-10 14:06:32 +0800

    修改 clinet Name 字段 長度

--- tbl/client_tbl.go
+++ tbl/client_tbl.go
@@ -2,7 +2,7 @@ package tbl
 
 type Client struct {
 	Id           int64  `gorm:"primaryKey;unique;not null;type:int(250)"`
-	Name         string `gorm:"type:varchar(20);comment:'名稱'"`
+	Name         string `gorm:"type:varchar(40);comment:'名稱'"`
 	ClientType   int64  `gorm:"type:int(12);comment:'商戶類別1:一般商戶 2: vip 商戶 3: 垃圾商戶'"`
 	ClientStatus int64  `gorm:"type:int(12);comment:'商戶狀態1:啟用 2:停用'"`
 	MerchantNo   string `gorm:"type:varchar(40);index:merchant_no,unique;comment:'商戶識別碼'"`
